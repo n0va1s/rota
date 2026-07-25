8fe7517ace792cfe9970400cc993d2ba832e7b37
Developer <dev@example.com>
2026-07-25 11:57:46 -0300

    chore: inclusao do laravel/boost

--- resources/views/livewire/layout/navigation.blade.php
+++ resources/views/livewire/layout/navigation.blade.php
@@ -35,7 +35,10 @@ $logout = function (Logout $logout) {
                 <x-dropdown align="right" width="48">
                     <x-slot name="trigger">
                         <button class="inline-flex items-center px-3 py-2 border border-transparent text-sm leading-4 font-medium rounded-md text-gray-500 bg-white hover:text-gray-700 focus:outline-none transition ease-in-out duration-150">
-                            <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
+                            <div class="flex flex-col items-end">
+                                <div x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
+                                <div class="text-xs text-gray-400">{{ auth()->user()->tip_role?->label() }}</div>
+                            </div>
 
                             <div class="ms-1">
                                 <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20">
@@ -84,7 +87,7 @@ $logout = function (Logout $logout) {
         <div class="pt-4 pb-1 border-t border-gray-200">
             <div class="px-4">
                 <div class="font-medium text-base text-gray-800" x-data="{{ json_encode(['name' => auth()->user()->name]) }}" x-text="name" x-on:profile-updated.window="name = $event.detail.name"></div>
-                <div class="font-medium text-sm text-gray-500">{{ auth()->user()->email }}</div>
+                <div class="font-medium text-sm text-gray-500">{{ auth()->user()->tip_role?->label() }} &middot; {{ auth()->user()->email }}</div>
             </div>
 
             <div class="mt-3 space-y-1">
